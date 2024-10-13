f788c2a2941cebf2e2e8a6a728201a701dd284c9
booky10 <boooky10@gmail.com>
2024-10-13 19:34:11 +0200

    Replace nbt-ts usage with own SNBT library

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,26 @@
 import { Express } from "express";
-import { parse, stringify } from "nbt-ts";
+import { parseTag } from "./parser.js";
 
 const DEFAULT_INDENT = 2;
+const MIN_INDENT = 0;
 const MAX_INDENT = 10;
 
-// TODO write own snbt parsing lib
 const nanos = (): number => Math.ceil(performance.now() * 1_000_000);
 const register = (app: Express) =>
   app.post("/api/v1/format", (req, res, next) => {
     const totalStart = nanos();
 
-    const indent = Math.min(MAX_INDENT, Number(req.query.indent) || DEFAULT_INDENT);
+    const suppliedIndent = Number(req.query.indent) || DEFAULT_INDENT;
+    const indent = Math.min(MAX_INDENT, Math.max(MIN_INDENT, suppliedIndent));
     res.header("Indent", `${indent}`);
 
     const parseStart = nanos();
-    const nbt = parse(req.body);
+    const nbt = parseTag(req.body);
     const parseDur = nanos() - parseStart;
     res.header("Parsing-Time", `${parseDur}`);
 
     const stringifyStart = nanos();
-    const stringifedNbt = stringify(nbt, { pretty: indent != 0 });
+    const stringifedNbt = nbt.asString(indent);
     const stringifyDur = nanos() - stringifyStart;
     res.header("Format-Time", `${stringifyDur}`);
 
