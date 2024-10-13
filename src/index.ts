8c0e0a7a3937629939f6b2a0af40748fb13aadae
booky10 <boooky10@gmail.com>
2024-10-13 05:18:46 +0200

    Setup initial express server

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import "dotenv/config.js";
+
+import express from "express";
+import cors from "cors";
+import { slowDown } from "express-slow-down";
+
+import routes from "./routes.js";
+
+const host = process.env.HOST || "0.0.0.0";
+const port = Number(process.env.PORT || 8080);
+const trustProxy = process.env.TRUST_PROXY || false;
+
+// express setup
+const app = express();
+app.disable("x-powered-by");
+if (trustProxy) {
+  console.warn("WARN: Will be trusting proxy fowarding headers");
+  app.set("trust proxy", true);
+}
+
+app.use(cors());
+app.use(
+  // allow 5 requests per 30s, then slow down
+  // add 200ms delay per each additional request
+  slowDown({
+    windowMs: 30 * 1000,
+    delayAfter: 5,
+    delayMs: (hits) => hits * 200,
+    validate: { trustProxy: false },
+  })
+);
+
+// register routes
+routes(app);
+
+// handle errors
+app.use((err, req, res, next) => {
+  console.error(`Received error from ${req.ip} with url ${req.url}`, err);
+  res.status(500).send({ status: 500, message: "server error" });
+});
+
+// boot process
+app.listen(port, host);
+console.log(`Startup done! Listening on ${port}`);
---
+++ src/routes.ts
@@ -0,0 +1,4 @@
+import { Express } from "express";
+
+const register = (app: Express) => app.get("/api/v1/format", (req, res, next) => {});
+export default register;
